065855b323a081defe09fbf57477b2cc6f726c75
Sauli Tähkäpää <sauli.tahkapaa@netti.fi>
2012-10-30 17:18:25 +0200

    Theres your problem!

--- TicTacToe/Tests.cs
+++ TicTacToe/Tests.cs
@@ -30,7 +30,7 @@ namespace TicTacToe
 
 		Func<Turn, bool> turnIsOnFirstHorizontalLine = t => t.Y == 1;
 		Func<Turn, bool> turnIsOnDiagonal = t => t.X == t.Y;
-		Func<Turn, bool> turnIsOnInvertedDiagonal = t => t.X == boardHeight - t.X + 1;
+		Func<Turn, bool> turnIsOnInvertedDiagonal = t => t.Y == boardHeight - t.X + 1;
 		Func<Turn, bool> turnIsOnLastVerticalLine = t => t.X == boardWidth;
 
 		[SetUp]
